1c4d3e0d10a1542c705ed4c7e4604a8da6e2b8b4
prerna-raj <prerna-raj@users.noreply.github.com>
2023-08-12 10:13:40 +0530

    added files to my repo

---
+++ index.js
@@ -0,0 +1,33 @@
+var typed = new Typed('#element', {
+    strings: ['Coder','Web Designer','Front-End Web Developer' ],
+    // strings: [' Front-End Web Development'],
+    typeSpeed: 100,
+  });
+  var tl=gsap.timeline()
+
+  tl.from("nav .logo,nav .nav-items",{
+      y:-100,
+      duration:1,
+      delay:0.5,
+      opacity:0,
+      stagger:0.3
+  })
+  tl.from(".left h1 , .btn ",{
+    x:-100,
+    opacity:0,
+    stagger:0.3
+  })
+  tl.from(".right img",{
+      scale:0,
+      opacity:0,
+      
+  })
+ tl.from(".scroll",{
+  y:30,
+  repeat:-1,
+  duration:0.6,
+  yoyo:true,
+  opacity:0
+ })
+ 
+  
